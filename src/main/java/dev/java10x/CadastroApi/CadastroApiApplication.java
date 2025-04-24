a436f813f1c7d41a70fe2921f8ddb9e66435b056
Lippo <ascendinomartins2303@gmail.com>
2025-04-24 16:13:13 -0300

    Criado a controller

--- src/main/java/dev/java10x/CadastroApi/CadastroApiApplication.java
+++ src/main/java/dev/java10x/CadastroApi/CadastroApiApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastroApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroApiApplication.class, args);
 	}
-
+²
 }
---
+++ src/main/java/dev/java10x/CadastroApi/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/myRoute")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem";
+    }
+
+}
